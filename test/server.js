3137d0d868fe62df28a38df103c33d08d3fb088f
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2019-09-14 19:41:09 -0400

    Text analysis and search confirmation added to server

--- test/server.js
+++ test/server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const language = require('@google-cloud/language');
 const request = require('request');
 const express = require('express');
 const FireEye = require('FireEye');
@@ -41,68 +41,119 @@ app.get('/', function(req, res) {
     res.render('./index.html');
 });
 
-// socket.on('search', (data) => {
-// 	search_item = data;
-// });
+
+var search_item = "unknown";
+
+io.on('connection', (ioSocket) => {
+
+	// Find item to search for
+	ioSocket.on('search', (data) => {
+		console.log(data);
+
+		textAnalysis(data)
+			.then(item => {
+
+				console.log(item);
+				search_item = item;
+				ioSocket.emit('search_item',item);
+			})
+			.catch(console.error);
+	});
+
+	ioSocket.on('confirmed_search', (data) => {
+		// Run the search!!!
+
+	});
+
+});
+
+
+async function textAnalysis(data){
+
+	// Instantiates a client
+	const client = new language.LanguageServiceClient();
+
+	// The text to analyze
+	const text = data;
+
+	const document = {
+		content: text,
+	 	type: 'PLAIN_TEXT',
+	};
+
+	// Detects the entities of the text
+	const [result] = await client.analyzeEntities({document: document});
+	const entities = result.entities;
+	const item = entities[0].name;
+
+	return item;
+
+};
+
+
+
 var x = 0;
+
+function analyzeImage(data){
+
+	let subscriptionKey = process.env['AZURE_KEY'];
+	let endpoint = process.env['AZURE_ENDPOINT'];
+	if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
+	
+	var uriBase = endpoint + 'vision/v2.0/analyze';
+
+	// const imageUrl =
+	//     'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+	// Request parameters.
+	const params = {
+	    'visualFeatures': 'Description,Tags',
+	    'details': '',
+	    'language': 'en'
+	};
+
+	var buf = Buffer.from(data, 'base64');
+	// console.log("hi");
+	// console.log(buf);
+
+	const options = {
+	    uri: uriBase,
+	    qs: params,
+	    body: buf,
+	    headers: {
+	        'Content-Type': 'application/octet-stream',
+	        'Ocp-Apim-Subscription-Key' : subscriptionKey
+	    }
+	};
+
+	// console.log(buf.toString());
+	console.log("Image sent");
+	request.post(options, (error, response, body) => {
+	  if (error) {
+	    console.log('Error: ', error);
+	    return;
+	  }
+
+	  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+	  console.log('JSON Response\n');
+	  console.log(jsonResponse);
+
+	  var tags=JSON.parse(body)['tags'];
+	  var hi_conf_objects = tags.filter(tag => tag.confidence > .8).map(tag=>tag.name);
+
+	  console.log(hi_conf_objects);
+	  if(hi_conf_objects.includes(search_item)){
+	  		console.log("Found a person");
+	  };
+
+	}); 
+}
 
 socket.on('image', (data) => {
  	io.emit('image', data);
 	x = x+ 1;
  	if(x % 10 == 0){
- 		
-	 	let subscriptionKey = process.env['AZURE_KEY'];
-	 	let endpoint = process.env['AZURE_ENDPOINT'];
-	 	if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
-	 
-	 	var uriBase = endpoint + 'vision/v2.0/analyze';
-
-	 	// const imageUrl =
-	 	//     'https://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
-
-	 	// Request parameters.
-	 	const params = {
-	 	    'visualFeatures': 'Description,Tags',
-	 	    'details': '',
-	 	    'language': 'en'
-	 	};
-
-	 	var buf = Buffer.from(data, 'base64');
-	 	var search_item = "bottle";
-	 	// console.log("hi");
-	 	// console.log(buf);
-
-	 	const options = {
-	 	    uri: uriBase,
-	 	    qs: params,
-	 	    body: buf,
-	 	    headers: {
-	 	        'Content-Type': 'application/octet-stream',
-	 	        'Ocp-Apim-Subscription-Key' : subscriptionKey
-	 	    }
-	 	};
-
-	 	// console.log(buf.toString());
-	 	console.log("Image sent");
-	 	request.post(options, (error, response, body) => {
-	 	  if (error) {
-	 	    console.log('Error: ', error);
-	 	    return;
-	 	  }
-
-	 	  let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-	 	  console.log('JSON Response\n');
-	 	  console.log(jsonResponse);
-
-	 	  var tags=JSON.parse(body)['tags'];
-	 	  var hi_conf_objects = tags.filter(tag => tag.confidence > .8).map(tag=>tag.name);
-
-	 	  console.log(hi_conf_objects);
-	 	  if(hi_conf_objects.includes(search_item)){
-	 	  		console.log("Found a person");
-	 	  };
-
-	 	}); 
-	 }
+		analyzeImage(data); 	
+	}
 
 });
